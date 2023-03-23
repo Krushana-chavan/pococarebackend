27156962ac46b70e599ee886029e2d2c73f9277e
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2023-03-24 04:20:29 +0530

    completed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,7 @@ const getUser = async (req, res) => {
     const user = await AuthModel.findById(id);
     res.status(200).json(user);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(401).json({ message: err.message });
   }
 };
 
